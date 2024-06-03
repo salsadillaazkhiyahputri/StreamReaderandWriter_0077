7b37fa5f33c03716931e34ed11f48bbbaa2f844a
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 15:18:50 +0700

    membuat obyek exception yang akan digunakan untuk awal program dengan pesan array integer 3 elemen sebagai penangkap menggunakan obyek exception bahwa program berjalan tanpa henti meskipun terjadi kesalahan

--- ExceptionStandard.cpp
+++ ExceptionStandard.cpp
@@ -19,6 +19,8 @@ int main ()
         /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
     }
     cout << "Baris Program Yang Terakhir" << endl;
+    /*penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan*/
+    return 0;
 }
 
 
