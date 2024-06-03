9f354561ef1932caee14eddd53bbc2d64f71097a
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 14:33:14 +0700

    loop akan berhenti jika memasukkan karakter q

--- iOFile.cpp
+++ iOFile.cpp
@@ -14,5 +14,9 @@ int main(){
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
     //unlimited loop untuk menulis
-    
-}
+    while (true){
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+       
