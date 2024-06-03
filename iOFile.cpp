75fbf39548f12f0fe0e7bd3ebc93d04d5d0c7f6d
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 14:27:16 +0700

    unlimited loop utk menulis

--- iOFile.cpp
+++ iOFile.cpp
@@ -11,4 +11,8 @@ int main(){
     //menunjuk ke sebuah nama file
     outfile.open("contohfile.txt");
 
-   
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop untuk menulis
+    
+}
