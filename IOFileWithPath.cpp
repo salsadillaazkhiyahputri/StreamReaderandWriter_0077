666eb187b0bab7aeb6a9ac7ecc0d197aaca68737
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 14:58:58 +0700

    IOFileWithPath

--- IOFileWithPath.cpp
+++ IOFileWithPath.cpp
@@ -18,4 +18,40 @@ int main(){
     cout << ">= Menulis file, \'q\' untuk keluar" << endl;
 
     //unlimited loop akan menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukkan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile + ".txt", ios::in);
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai 
+        infile.close();
+    }
+    //jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+
+}
     
