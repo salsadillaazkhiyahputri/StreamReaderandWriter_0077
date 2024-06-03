35885aad638540a0a818d1f3b430d69542627be1
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 14:37:48 +0700

    menulis dan memasukkan nilai dari baris ke dalam file

--- iOFile.cpp
+++ iOFile.cpp
@@ -19,4 +19,8 @@ int main(){
         //mendapatkan setiap karakter dalam satu baris
         getline(cin, baris);
         //loop akan berhenti jika anda memasukkan karakter q
-       
+        if (baris == "q") break;
+        //menulis dan memasukkan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
+    
