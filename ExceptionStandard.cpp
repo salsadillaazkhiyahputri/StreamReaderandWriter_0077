7242409fd35e78a66a0030b4a2977007b99ff4a4
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 15:14:05 +0700

    membuat program manual exception

---
+++ ExceptionStandard.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan 
+#include <array>
+//untuk obyek array yang akan kita gunakan 
+using namespace std;
+
+int main ()
+{
+    cout << "Awal Program" << endl; //penanda 1:..
+    try{
+        array<int, 3> data= {8,4,2};
+        //pesan array integer 3 elemen
+        cout << data.at(5)<<endl;
+    }
+    catch (exception& e){
+        //penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
+    cout << "Baris Program Yang Terakhir" << endl;
+}
+
+
