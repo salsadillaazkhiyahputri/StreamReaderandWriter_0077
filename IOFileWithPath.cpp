555fea6f78df3e53389c021e27bf634bae8c5c3b
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2024-06-03 14:53:24 +0700

    unlimited file loop akan menulis

--- IOFileWithPath.cpp
+++ IOFileWithPath.cpp
@@ -15,4 +15,7 @@ int main(){
     //menunjuk ke sebuah nama file
     outfile.open(NamaFile + ".txt", ios::out);
 
-   
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+    //unlimited loop akan menulis
+    
